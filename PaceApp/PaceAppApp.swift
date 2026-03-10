266c2784d8878628c659622ca8afc57445693bcb
FURKAN VIJAPURA <fvijapura@wvelabs.com>
2026-03-10 17:31:02 +0530

    Initial project setup - In Progress

---
+++ PaceApp/PaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceAppApp.swift
+//  PaceApp
+//
+//  Created by FURKAN VIJAPURA on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct PaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
