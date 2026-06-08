a46b6672e719290b1464d11cdde100a50716347d
FURKAN VIJAPURA <fvijapura@wvelabs.com>
2026-06-08 16:02:25 +0530

    Update AuthenticatingScreen.swift

--- Modules/Auth/Authenticating/AuthenticatingScreen.swift
+++ Modules/Auth/Authenticating/AuthenticatingScreen.swift
@@ -11,28 +11,29 @@ struct AuthenticatingScreen: View {
 
     var body: some View {
         BackgroundContainer {
-            VStack(spacing: 24) {
+            VStack(spacing: 46) {
                 Spacer()
 
-				Text(.paceApp)
-					.font(.extraBold34)
-					.foregroundStyle(.whiteApp)
-					.tracking(2)
-                    .scaleEffect(showUI ? 1 : 0.85)
-                    .opacity(showUI ? 1 : 0)
-                    .animation(.spring(response: 0.45, dampingFraction: 0.8), value: showUI)
-
-				VSpace(height: 12)
+//				Text(.paceApp)
+//					.font(.extraBold34)
+//					.foregroundStyle(.whiteApp)
+//					.tracking(2)
+//                    .scaleEffect(showUI ? 1 : 0.85)
+//                    .opacity(showUI ? 1 : 0)
+//                    .animation(.spring(response: 0.45, dampingFraction: 0.8), value: showUI)
+//
+//				VSpace(height: 12)
 
                 VStack(spacing: 46) {
                     ProgressView()
-						.tint(.neonAquaBlue)
+						.tint(.whiteApp)
                         .scaleEffect(1.5)
                     
                     Text("Authenticating...")
 						.font(.medium24)
+						.tracking(1.5)
                         .foregroundStyle(.whiteApp)
-                        .opacity(showUI ? 0.9 : 0)
+                        .opacity(showUI ? 0.85 : 0)
                         .animation(.easeIn(duration: 0.3).delay(0.2), value: showUI)
                 }
                 .padding(.bottom, 48)
